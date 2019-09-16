ec4613791edd7d2c7eba375eba14a149627847a5
Mai Abd Elmonem <mai.abdelmonem@admins-MacBook-Pro.local>
2019-09-16 12:28:45 +0200

    mvp archticture

---
+++ Movies Home/Features/ActorList/Presenter/ActorListPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  ActorListPresenter.swift
+//  Movies Home
+//
+//  Created by Mai Abd Elmonem on 9/16/19.
+//  Copyright © 2019 Mai Ahmed. All rights reserved.
+//
+
+import Foundation
+
+class ActorListPresenter: ActorViewProtocol, ActorModelProtocol {
+    
+}
---
+++ Movies Home/Features/ActorList/View/ViewProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ViewProtocol.swift
+//  Movies Home
+//
+//  Created by Mai Abd Elmonem on 9/16/19.
+//  Copyright © 2019 Mai Ahmed. All rights reserved.
+//
+
+import Foundation
+
+protocol ActorViewProtocol {
+    
+}
