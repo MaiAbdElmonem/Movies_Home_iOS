2b0193842af9a1d846c96917a2e0a333a97b515a
mai ahmed <maiahmed@192.168.220.128>
2019-10-12 17:33:24 -0700

    codable

--- Movies Home/Features/ActorList/Model/Actor.swift
+++ Movies Home/Features/ActorList/Model/Actor.swift
@@ -9,51 +9,66 @@
 import Foundation
 import ObjectMapper
 
-struct ActorApiResponse : Mappable {
-    var totalResults : Int?
-    var results : [Actor]?
-
-    init?(map: Map) {
-
-    }
-
-    mutating func mapping(map: Map) {
-        totalResults <- map["total_results"]
-        results <- map["results"]
-    }
-
-
-}
-
-struct Actor : Mappable {
-    var name: String?
-    var profilepath : String?
+struct Actor : Codable {
     var id : Int?
+    var profilepath : String?
+    var name: String?
     
-    init(id:Int, profilepath:String, name:String) {
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case profilepath = "profile_path"
+        case name = "name"
+    }
+    
+    init(from decoder : Decoder) throws {
+        <#statements#>
+    }
+    
+    init(id: Int, profilepath: String, name:String) {
         self.id = id
-        self.name = name
         self.profilepath = profilepath
+        self.name = name
     }
-    
-    init?(map: Map) {
-        
-    }
-    
-    mutating func mapping(map: Map) {
-        //string maps api
-        id <- map["id"]
-        name <- map["name"]
-        profilepath <- map["profile_path"]
-    }
-    
-    
-    
 }
-
-//class Actor {
-//    var name: String = ""
-//    var profile_path:String=""
-//    var id=Int()
+//struct ActorApiResponse : Mappable {
+//    var totalResults : Int?
+//    var results : [Actor]?
+//
+//    init?(map: Map) {
+//
+//    }
+//
+//    mutating func mapping(map: Map) {
+//        totalResults <- map["total_results"]
+//        results <- map["results"]
+//    }
+//
 //
 //}
+
+//struct Actor : Mappable {
+//    var name: String?
+//    var profilepath : String?
+//    var id : Int?
+//
+//    init(id:Int, profilepath:String, name:String) {
+//        self.id = id
+//        self.name = name
+//        self.profilepath = profilepath
+//    }
+//
+//    init?(map: Map) {
+//
+//    }
+//
+//    mutating func mapping(map: Map) {
+//        //string maps api
+//        id <- map["id"]
+//        name <- map["name"]
+//        profilepath <- map["profile_path"]
+//    }
+//
+
+    
+}
+
