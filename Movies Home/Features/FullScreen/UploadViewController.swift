16fbe10b4226ffb8625152b76e869f0945ffd84a
mai ahmed <maiahmed@192.168.220.128>
2019-10-16 14:03:10 -0700

    pickImage

--- Movies Home/Features/FullScreen/UploadViewController.swift
+++ Movies Home/Features/FullScreen/UploadViewController.swift
@@ -10,15 +10,41 @@ import UIKit
 import OAuthSwift
 import SWXMLHash
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var showimage: UIImageView!
+    
+    var auth = OAuth1Swift()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imagePickerController.delegate = self
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func PickImage(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            print("Button capture")
+            imagePicker.sourceType = .savedPhotosAlbum
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismiss(animated: true, completion: { () -> Void in
+            
+        })
+        
+        showimage.image = image
+    }
+    
+    @IBAction func UploadImage(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
